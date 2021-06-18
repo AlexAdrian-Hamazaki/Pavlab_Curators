22fbe35c3b7419fa9e7ad4bdab3443c35ba1dd89
Alex Adrian-Hamazaki <aadrian@nelson.msl.ubc.ca>
2021-06-18 12:16:50 -0700

    gitignore

--- GemmaPipeLine/curationPipe.sh
+++ GemmaPipeLine/curationPipe.sh
@@ -1,20 +1,21 @@
 #!bin/bash
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+SCRIPT_DIR=“$( cd “$( dirname “${BASH_SOURCE[0]}” )” &> /dev/null && pwd )”
 cd $SCRIPT_DIR
-while IFS= read -r line || [ -n "$line" ]
+while IFS= read -r line || [ -n “$line” ]
 do
-	gse=$(awk '{print $1}' <<< "$line")
+	gse=$(awk ‘{print $1}’ <<< “$line”)
 	python batchChecking.py $gse $GEMMAUSERNAME $GEMMAPASSWORD
-	for line in $(grep . "temp.txt")
-		if [[ "$line" == "True" ]]
+	for state in $(grep . “temp.txt”)
+	do
+		if [[ “$state” == “True” ]]
 			then
 				$GEMMACMD makeProcessedData -u $GEMMAUSERNAME -p $GEMMAPASSWORD -e $gse;
 			else
-				echo no batch to worried about for $gse, moving on 
+				echo no batch to worried about for $gse, moving on
 		fi
 	done
 		rm temp.txt	
 		$GEMMACMD diffExAnalyze -u $GEMMAUSERNAME -p $GEMMAPASSWORD -e $line;
 		$GEMMACMD generateDataFile -u $GEMMAUSERNAME -p $GEMMAPASSWORD -e $gse;
 done < curatingList.txt
-python eeID.py 'curatingList.txt' $GEMMAUSERNAME $GEMMAPASSWORD;
+python eeID.py ‘curatingList.txt’ $GEMMAUSERNAME $GEMMAPASSWORD;
