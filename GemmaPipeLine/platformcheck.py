bae9b7949fc8e71c0533d815be8073c1b7a761d2
WilsonTu <s31302@gmail.com>
2021-06-24 11:45:16 -0700

    adding a script for scraping platforms

--- GemmaPipeLine/platformcheck.py
+++ GemmaPipeLine/platformcheck.py
@@ -1,8 +1,12 @@
 #"""
 # Linear operation of systematically find out which experiemnts require attention for outlier (possible to optimize runtime using multiprocessing module)
 #"""
-
+"""
+Concurrent.futures pool executor worth considering if the task gets bigger.
+Purpose: This script simply takes an input of GSEs from curationList.txt to get a list of platform into platform.txt
+"""
 import json
+from os import error
 import urllib2
 import base64
 import sys
@@ -11,35 +15,43 @@ import re
 
 def main():
     # Without this the urllib2.request would fail
-    ssl._create_default_https_context = ssl._create_unverified_context
+	ssl._create_default_https_context = ssl._create_unverified_context
 
     # Change this if you want to simutaneously process more than 50 GSEs
-    limit = 100
+	limit = 100
 
-    username = sys.argv[2]
-    password = sys.argv[3]
-    base64string = base64.encodestring('{}:{}'.format(username, password))[:-1]
-    
-    
-    GSE = str(sys.argv[1])
+	username = sys.argv[2]
+	password = sys.argv[3]
 
-    print ('Start scrapping using the Gemma REST_api')
-    plat= "https://gemma.msl.ubc.ca/rest/v2/datasets/{}/platforms".format(GSE)
-    # Add basic authentication header
-    request = urllib2.Request(plat)
-    request.add_header("Authorization", "Basic {}".format(base64string)) 
-    response = urllib2.urlopen(request)
-    text = response.read()
-    platform_dict = json.loads(text)
-    platform=" "
-    if re.match("^Affy", platform_dict[u'data'][0][u'name']):
-        platform = "affy"
-    
-    
+	print('Start scrapping using the Gemma REST_api')
+	platforms=[]
+	with open(sys.argv[1], "r") as f:
+		for line in f:
+			while True:
+				try:
+					GSE=line.strip()
+					plat= "https://gemma.msl.ubc.ca/rest/v2/datasets/{}/platforms".format(GSE)
+					# Add basic authentication header
+					request = urllib2.Request(plat)
+					base64string = base64.encodestring('{}:{}'.format(username, password))[:-1]
+					request.add_header("Authorization", "Basic {}".format(base64string))
+					response = urllib2.urlopen(request)
+					text = response.read()
+					platform_dict = json.loads(text)
+					platforms.append(platform_dict[u'data'][0][u'name'].encode('ascii', 'ignore'))
+				except urllib2.HTTPError:
+					break
+				except error:
+					exit("something happend {}".format(error))
+				break
+					
+				
+		    
+	with open('platform.txt', 'w') as f:
+	    for platform in platforms:
+        	f.write(platform + "\n")
 
-    with open('temp.txt', 'w') as f:
-        f.write(state + "   " + platform)
-	
+   
 
 if __name__ == "__main__":
     main()
