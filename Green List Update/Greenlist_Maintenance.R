a36889dcf9bc88586b596f78d879c9f8365dc39b
WilsonTu <s31302@gmail.com>
2021-06-25 16:14:48 -0700

    added output message for greenlist R script

--- Green List Update/Greenlist_Maintenance.R
+++ Green List Update/Greenlist_Maintenance.R
@@ -1,11 +1,12 @@
 # Loading Libraries
-list.of.packages <- c("googlesheets4", "rstudioapi", "gtools")
+list.of.packages <- c("googlesheets4", "rstudioapi", "gtools", "tidyverse")
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
 if(length(new.packages)) install.packages(new.packages)
 library(data.table)
 library(gtools)
 library(rstudioapi)
 library(googlesheets4)
+library(tidyverse)
 #########################
 
 #Purpose: Confirm that experiments on the temporary greenlist are greenlisted, and to see if any of those experiements have problems being greenlisted
@@ -21,7 +22,7 @@ library(googlesheets4)
 
 #*****************************Change variables here******************************
 url_to_sheet = "https://docs.google.com/spreadsheets/d/17xm2eFFqhhT-M6-jTC_lsar7RMgk8Ln-TwQPDWlRfIY/edit?ts=5744cba9#gid=956906386"
-sheet_name ="[April 2021] Curation Complete (Green List)"
+sheet_name ="[June2021] Curation Complete (Green List)"
  #********************************************************************************
 ## Read From sheet name [April 2021] Curation Complete (Green List) on google sheets. Change the link and name of sheet accordingly
 greenTable <- read_sheet(url_to_sheet,
@@ -31,11 +32,14 @@ setDT(greenTable)
 #greenTable = fread(######'/home/user/curation_complete_list.tsv######, sep = '\t', header = TRUE, select = 1:2)
 eeTable = fread('/space/grp/nlim/CronGemmaDump/EE_Dump.TSV', sep = '\t', header = TRUE, quote = '')
 eeTable = eeTable[, .(ee.ID, ee.Name, ee.OriginalID)]
-
 ### checking what the largest GSE in gemma is
 #eeTable_above = dplyr::select(eeTable, ee.OriginalID)
 #eeTable_above = mixedsort(eeTable_above)
 #mixedsort(eeTable$ee.OriginalID, decreasing=TRUE)
+
+reason_master <- data.table(ee.ID=double(),
+                 ee.Name=character(), 
+                 Reason=character()) 
 
 # 1. Existence Check
 IDVector = greenTable$ee.ID %in% eeTable$ee.ID
@@ -54,7 +58,10 @@ problemID_1 = greenTable[existence_disagreement & IDVector, ee.ID]
 problemName = greenTable[existence_disagreement & nameVector, ee.Name]
 problemID_2 = eeTable[.(problemName), ee.ID, on = 'ee.Name']
 rm(problemName)
-
+mis_match <- unique(c(problemID_1,problemID_2))
+mis_match <- data.table(ee.ID=mis_match, reason="ee.ID and GSE are not correctly matched. Double check")
+reason_master <- mis_match
+rm(mis_match)
 # Problem Case 3: Both IDs and Names do not exist in Gemma --> Non-issue
 ## NO CODE
 
@@ -72,11 +79,17 @@ problemID_3 = table_duplicated_IDs[, ee.ID]
 tempNameVector = greenTable[table_duplicated_IDs, ee.Name, on = 'ee.ID']
 problemID_3A = eeTable[tempNameVector, ee.ID, on = 'ee.Name']
 rm(tempNameVector)
+ee_duplicate <- unique(c(problemID_3,problemID_3A))
+ee_duplicate <- data.table(ee.ID=ee_duplicate, reason="ee.ID duplicated. Double check")
+reason_master <- left_join(reason_master, ee_duplicate, by="ee.ID")
 
 # 3B. Same as 3A, but for duplicated Names
 table_duplicated_Names = greenTable[, .N, by = ee.Name][N > 1]
 problemID_4 = greenTable[table_duplicated_Names, ee.ID, on = 'ee.Name']
 problemID_4A = eeTable[table_duplicated_Names$ee.Name, ee.ID, on = 'ee.Name']
+name_duplicate <- unique(c(problemID_4,problemID_4A))
+name_duplicate <- data.table(ee.ID=name_duplicate, reason="GSE name duplicated. Double check")
+reason_master <- left_join(reason_master, name_duplicate, on="ee.ID")
 
 # Update GreenList for non-duplicates
 greenTable = greenTable[!(ee.ID %in% table_duplicated_IDs$ee.ID)][!(ee.Name %in% table_duplicated_Names$ee.Name)]
@@ -90,7 +103,9 @@ problem_table = greenTable[name_mismatch_vector | id_mismatch_vector]
 problemID_5 = problem_table$ee.ID
 problemID_5A = eeTable[problem_table, ee.ID, on = 'ee.Name']
 rm(problem_table)
-
+final_test <- unique(c(problemID_5,problemID_5A))
+final_test <- data.table(ee.ID=final_test, reason="Didn't pass the final check. ee.ID and GSE name not matched in GEMMA")
+reason_master <- left_join(reason_master, final_test, on="ee.ID")
 # Update GreenList for non-mismatches, sort by ID
 greenTable = greenTable[!(name_mismatch_vector | id_mismatch_vector)]
 setorder(greenTable, ee.ID)
@@ -108,11 +123,12 @@ problemID_ALL = unique(c(
 	problemID_5,
 	problemID_5A
 ))
-recurationTable = eeTable[.(problemID_ALL), .(ee.ID, ee.Name), on = 'ee.ID']
+#recurationTable = eeTable[.(problemID_ALL), .(ee.ID, ee.Name), on = 'ee.ID']
+reason_master = eeTable[reason_master , on = 'ee.ID']
 x <- dirname(getActiveDocumentContext()$path)
 setwd(x)
 #write.table(recurationTable, file = 'OneOffScripts/Curation/Recuration.tsv', sep = '\t', quote = FALSE, col.names = TRUE, row.names = FALSE)
-write.table(recurationTable, file = "Recuration.tsv", sep = '\t', quote = FALSE, col.names = TRUE, row.names = FALSE)
+write.table(reason_master, file = "Recuration.tsv", sep = '\t', quote = FALSE, col.names = TRUE, row.names = FALSE)
 
 # Dump out Cleaned GreenList
 write.table(greenTable, file = "cleanGreenList.tsv", sep = '\t', quote = FALSE, col.names = TRUE, row.names = FALSE)
