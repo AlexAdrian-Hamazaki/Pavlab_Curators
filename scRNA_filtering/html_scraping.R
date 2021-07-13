a3ad828ca1b63fa1359cd6a8d8f8ce530f4c6f23
AlexAdrian-Hamazaki <alexadrian@hotmail.ca>
2021-07-13 15:33:40 -0700

    html scraping

--- scRNA_filtering/html_scraping.R
+++ scRNA_filtering/html_scraping.R
@@ -4,9 +4,15 @@
 packages <- c(
               "tidyverse",
               "data.table",
-              "rstudioapi")
+              "rstudioapi",
+              "rvest")
 
 new_packages <- setdiff(packages, installed.packages()[,"Package"])
 install.packages(new_packages)
 
 #--- 
+
+library(rvest)
+
+
+sc_geo <- read_html("https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE116470")
