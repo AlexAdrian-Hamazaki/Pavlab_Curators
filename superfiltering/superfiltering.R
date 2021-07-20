ed4805bc1a05f6bce47feeecacd3fce6e9eb86af
AlexAdrian-Hamazaki <alexadrian@hotmail.ca>
2021-07-20 15:03:45 -0700

    halfway done changing this

--- superfiltering/superfiltering.R
+++ superfiltering/superfiltering.R
@@ -1,4 +1,10 @@
-list.of.packages <- c("tidyverse", "dplyr", "data.table", "rentrez", "googlesheets4", "rstudioapi")
+list.of.packages <- c("tidyverse",
+                      "dplyr",
+                      "data.table",
+                      "rentrez",
+                      "googlesheets4",
+                      "rstudioapi")
+
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
 if(length(new.packages)) install.packages(new.packages)
 
@@ -8,24 +14,35 @@ library(data.table)
 library(googlesheets4)
 library(rvest)
 library(rstudioapi)
+library(rentrez)
+library(stringr)
+
 x <- dirname(getActiveDocumentContext()$path)
 setwd(x)
+
 ## Read From sheet name SubSuperFiltered on google sheets. Change the link and name of sheet accordingly
 raw_list <- read_sheet("https://docs.google.com/spreadsheets/d/15w03FK5pzr19yqReWLV_Kai5w0740N91RyNbjDscziY/edit#gid=287890379",
                        sheet = "SubSuperFiltered")
 
-library(rentrez)
-library(stringr)
 new_vector <- c()
 ## Function that checks if a GSE is superseries. If it is we will fetch all it's subseries
 get_sub <- function(gses){
+  
   sapply(gses, function(gse){
+    
+    # First use the entrez_search API to check the if the GSE is a supseries
     super <- entrez_search(db="gds", term=paste(gse,"[ACCN]"), retmax=1)
     super_sum <- entrez_summary("gds", id =super$ids)
+    
+    # If GSE is a supseries, download the HTML. Search for "td" nodes
     if (grepl(" SuperSeries ", super_sum$summary)){
+      message(paste(gse, "is a Superseries. Checking for its subseries..."))
       url <- paste0("https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=", gse)
-      raw <- read_html(url)
-      text <- raw %>% html_text()
+      gse_html <- read_html(url) %>%
+        html_nodes("td") %>%
+        html_text()
+      browser()qqqqqqq
+      subseries
       GSEs <- str_extract_all(text, '\\bGSE\\d+')
       GSEs <- strsplit(GSEs[[1]], " ")
       GSEs <- unique(GSEs)
