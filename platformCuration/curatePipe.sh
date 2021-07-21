7d133e1574b7cf8989ab12d373c118c3201e59d8
WilsonTu <s31302@gmail.com>
2021-07-21 14:36:04 -0700

    fixing small mistakes with downloadPie

--- platformCuration/curatePipe.sh
+++ platformCuration/curatePipe.sh
@@ -3,8 +3,8 @@ echo "what is the GPL number? (enter in the form as GPLXXXX)"
 read gpl
 $GEMMACMD addPlatformSequences -u $GemmaUsername -p $GEMMAPASSWORD -a $gpl -y OLIGO -f ${gpl}.probetab
 $GEMMACMD blatPlatform -u $GemmaUsername -p $GEMMAPASSWORD --array $gpl
+$GEMMACMD mapPlatformToGenes -u $GEMMAUSERNAME -p $GEMMAPASSWORD --array $gpl -force
 $GEMMACMD makePlatformAnnotFiles -u $GemmaUsername -p $GEMMAPASSWORD -a $gpl
-$GEMMACMD mapPlatformToGenes -u $GEMMAUSERNAME -p $GEMMAPASSWORD --array $gpl
 rm raw.${gpl}.probetab
 rm noSequence_${gpl}.probetab
 rm ${gpl}.probetab
--- platformCuration/downloadPipe.sh
+++ platformCuration/downloadPipe.sh
@@ -13,7 +13,7 @@ echo which column is Sequence ID
 read sequenceID
 echo which column is Sequence
 read sequence
-cat platform.txt | awk -F '\t' -v "col1=$elementID" -v "col2=$sequenceID" -v "col3=$sequence" '{print $col1 "\t" $col2 "\t" $12}' > raw.${gpl}.probetab
+cat platform.txt | awk -F '\t' -v "col1=$elementID" -v "col2=$sequenceID" -v "col3=$sequence" '{print $col1 "\t" $col2 "\t" $col3}' > raw.${gpl}.probetab
 cat raw.${gpl}.probetab | awk -F '\t' 'NR>1 && $3 ~/[A-Z]{4}$/' > ${gpl}.probetab ; cat raw.${gpl}.probetab | awk -F '\t' 'NR>1 && !$3' > noSequence_${gpl}.probetab
 rm platform.txt
 sed -i '/\"/d' ${gpl}.probetab 
